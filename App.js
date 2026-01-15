7c4f366eecad4983f74171310198a197ba9edde9
huizh11 <hanghuizhi@gmail.com>
2026-01-15 14:43:00 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//Create a new variable named OriginalData
+let originalData = []
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    const [mydata, setMydata] = useState([]);
+
+    useEffect(() => {
+        // Add fetch() - Exercise 1A
+        const myurl = "https://sanriocharacters.onrender.com/allcharacters";
+
+        fetch(myurl)
+            .then((response) => response.json())
+            .then((myJson) => {
+                setMydata(myJson);
+                originalData = myJson;
+            });
+    }, []); //
+
+    const FilterData = (text) => {
+        if (text !== '') {
+            let myFilteredData = originalData.filter((item) =>
+                item.character_name.toLowerCase().includes(text.toLowerCase())
+            );
+            setMydata(myFilteredData);
+        } else {
+            setMydata(originalData);
+        }
+    };
+
+
+    const renderItem = ({ item }) => {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}
+            >
+                <Text style={{ fontSize: 16 }}>
+                    {item.character_name}
+                </Text>
+
+                <Image
+                    source={{ uri: item.character_pic }}
+                    style={{ width: 80, height: 110 }}
+                    resizeMode="contain"
+                />
+
+            </View>
+        );
+    };
+
+
+    return (
+        <View>
+            <StatusBar />
+            <Text>Search:</Text>
+            <TextInput
+                style={{ borderWidth: 1, marginTop: 10 }}
+                onChangeText={(text) => { FilterData(text); }}
+            />
+            <FlatList data={mydata} renderItem={renderItem} />
+        </View>
+    );
+};
+
+export default App;
